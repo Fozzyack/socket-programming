216d27ce92cac03179fb065ff41c546b8f1097f3
Frasier <fsundra@gmail.com>
2025-11-25 00:00:05 +0800

    Adds a simple server configuration - Sets up Socket - Sets up client listener (thing?) - bind - listens - accepts connections - sets up the client file descriptor and returns (though this is   probably the wrong way to go about it)

--- src/main.c
+++ src/main.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 
-int main() {
+#include <sys/socket.h>
+#include <netinet/in.h>
 
+#define PORT 9191
+#define BACKLOG 5
+
+int start_server(void) {
+    // Your code here
     struct sockaddr_in server_info = {0};
+    struct sockaddr_in client_info = {0};
+
+
+    socklen_t client_size = sizeof(client_info);
+
     server_info.sin_family = AF_INET;
-    server_info.sin_addr.s_addr = 0;
-    server_info.sin_port = htons(5555);
+    server_info.sin_addr.s_addr = INADDR_ANY;
+    server_info.sin_port = htons(PORT);
+
+
 
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd == -1) {
@@ -18,14 +28,35 @@ int main() {
         return -1;
     }
 
-    if(bind(fd, (struct sockaddr *)&server_info, sizeof(server_info)) == -1) {
+    if (bind(fd, (struct sockaddr *) &server_info, sizeof(server_info)) == -1) {
         perror("bind");
         close(fd);
         return -1;
     }
+    
+
+    if (listen(fd, 5) == -1) {
+        perror("listen");
+        close(fd);
+        return -1;
+    }
+
+    int cfd = accept(fd, (struct sockaddr *)&client_info, &client_size);
+    if (cfd == -1) {
+        perror("accept");
+        close(fd);
+        return -1;
+
+    }
+
+    return cfd;
+}
 
 
-    close(fd);
-
+int main() {
+    int fd = start_server();
+    if (fd != -1) {
+        close(fd);
+    }
     return 0;
 }
